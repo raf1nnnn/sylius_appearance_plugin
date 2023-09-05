1365af255321bb6ecf5e95fe405ea83287aceaae
MorsiBouhlel <31985111+MorsiBouhlel@users.noreply.github.com>
2023-09-05 23:12:11 +0100

    FooterListNormalizer

---
+++ src/Serializer/FooterListNormalizer.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dotit\SyliusAppearancePlugin\Serializer;
+
+use Dotit\SyliusAppearancePlugin\Entity\FooterListInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Webmozart\Assert\Assert;
+
+final class FooterListNormalizer implements ContextAwareNormalizerInterface, NormalizerAwareInterface
+{
+    private NormalizerInterface $normalizer;
+
+    public function __construct(NormalizerInterface $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
+    private const ALREADY_CALLED = 'footer_list_normalizer_already_called';
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        Assert::isInstanceOf($object, FooterListInterface::class);
+        Assert::keyNotExists($context, self::ALREADY_CALLED);
+
+        $context[self::ALREADY_CALLED] = true;
+
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        foreach ($data['footerListDetails'] as $key => $value) {
+            if (!$value['enabled']) {
+                    array_splice($data['footerListDetails'], $key, 1);
+            }
+        }
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null, $context = []): bool
+    {
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
+        return $data instanceof FooterListInterface;
+    }
+
+    /**
+     * @param NormalizerInterface $normalizer
+     */
+    public function setNormalizer(NormalizerInterface $normalizer): void
+    {
+        $this->normalizer = $normalizer;
+    }
+}
